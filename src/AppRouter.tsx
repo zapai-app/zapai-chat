f7a22f7b65ba8a5f77464cb35bbc027dbbe98863
Milad Raeisi <nostrbot@gmail.com>
2025-11-06 08:54:35 +0400

    chore: remove markdown test files and routes
    - Remove MarkdownTest page and SimpleMarkdownTest component
    - Remove /markdown-test route from AppRouter
    - Clean up test-related imports

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -5,7 +5,6 @@ import Index from "./pages/Index";
 import { NIP19Page } from "./pages/NIP19Page";
 import { Settings } from "./pages/Settings";
 import { Wallet } from "./pages/Wallet";
-import { MarkdownTestPage } from "./pages/MarkdownTest";
 import NotFound from "./pages/NotFound";
 
 export function AppRouter() {
@@ -22,7 +21,6 @@ export function AppRouter() {
         <Route path="/" element={<Index />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/wallet" element={<Wallet />} />
-        <Route path="/markdown-test" element={<MarkdownTestPage />} />
         {/* NIP-19 route for npub1, note1, naddr1, nevent1, nprofile1 */}
         <Route path="/:nip19" element={<NIP19Page />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- src/pages/MarkdownTest.tsx
+++
@@ -1,9 +0,0 @@
-import { SimpleMarkdownTest } from '@/components/SimpleMarkdownTest';
-
-export function MarkdownTestPage() {
-  return (
-    <div className="min-h-screen bg-background text-foreground p-8">
-      <SimpleMarkdownTest />
-    </div>
-  );
-}
